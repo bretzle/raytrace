989cd1e153bf5ffdc6e99aeb2b88829776fa5807
Bretz <bretzj@ad.msoe.edu>
2020-11-25 22:02:44 -0600

    final render

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::rc::Rc;
 
 use camera::Camera;
 use color::write_color;
-use material::{Dielectric, Lambertian, Metal};
+use material::{Dielectric, Lambertian, Material, Metal};
 use objects::{list::HittableList, sphere::Sphere, HitRecord, Hittable};
 use ray::Ray;
 use utils::*;
@@ -16,61 +16,29 @@ mod ray;
 mod utils;
 mod vec3;
 
-const ASPECT_RATIO: f64 = 16.0 / 9.0;
-const IMAGE_WIDTH: i32 = 400;
+const ASPECT_RATIO: f64 = 3.0 / 2.0;
+const IMAGE_WIDTH: i32 = 1200;
 const IMAGE_HEIGHT: i32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as i32;
-const SAMPLES_PER_PIXEL: i32 = 100;
+const SAMPLES_PER_PIXEL: i32 = 50;
 const MAX_DEPTH: i32 = 50;
 
 fn main() {
 	// World
-	let mut world = HittableList::new();
-
-	let material_ground = Rc::new(Lambertian::new(Color::from(0.8, 0.8, 0.0)));
-	let material_center = Rc::new(Lambertian::new(Color::from(0.1, 0.2, 0.5)));
-	let material_left = Rc::new(Dielectric::new(1.5));
-	let material_right = Rc::new(Metal::new(Color::from(0.8, 0.6, 0.2), 0.0));
-
-	world.add(Rc::new(Sphere::new(
-		Point3::from(0.0, -100.5, -1.0),
-		100.0,
-		material_ground,
-	)));
-	world.add(Rc::new(Sphere::new(
-		Point3::from(0.0, 0.0, -1.0),
-		0.5,
-		material_center,
-	)));
-	world.add(Rc::new(Sphere::new(
-		Point3::from(-1.0, 0.0, -1.0),
-		0.5,
-		material_left.clone(),
-	)));
-	world.add(Rc::new(Sphere::new(
-		Point3::from(-1.0, 0.0, -1.0),
-		-0.45,
-		material_left,
-	)));
-	world.add(Rc::new(Sphere::new(
-		Point3::from(1.0, 0.0, -1.0),
-		0.5,
-		material_right,
-	)));
+	let world = random_scene();
 
 	// Camera
-	let lookfrom = Point3::from(-2.0, 2.0, 1.0);
-	let lookat = Point3::from(0.0, 0.0, -1.0);
+	let lookfrom = Point3::from(13.0, 2.0, 3.0);
+	let lookat = Point3::from(0.0, 0.0, 0.0);
 	let vup = Vec3::from(0.0, 1.0, 0.0);
 	let vfov = 20.0;
-	let aspect_ratio = 16.0 / 9.0;
-	let aperature = 2.0;
+	let aperature = 0.1;
 
 	let camera = Camera::new(
 		lookfrom,
 		lookat,
 		vup,
 		vfov,
-		aspect_ratio,
+		ASPECT_RATIO,
 		aperature,
 		(lookfrom - lookat).length(),
 	);
@@ -97,6 +65,72 @@ fn main() {
 	eprintln!("\nDone.");
 }
 
+fn random_scene() -> HittableList {
+	let mut world = HittableList::new();
+
+	let ground_material = Rc::new(Lambertian::new(Color::from(0.5, 0.5, 0.5)));
+	world.add(Rc::new(Sphere::new(
+		Point3::from(0.0, -1000.0, 0.0),
+		1000.0,
+		ground_material,
+	)));
+
+	for a in -11..11 {
+		for b in -11..11 {
+			let choose_mat = random(0., 1.);
+			let center = Point3::from(
+				a as f64 + 0.9 * random(0., 1.),
+				0.2,
+				b as f64 + 0.9 * random(0., 1.),
+			);
+
+			if (center - Point3::from(4., 0.2, 0.)).length() > 0.9 {
+				let mut sphere_material: Rc<dyn Material> =
+					Rc::new(Metal::new(Color::default(), 0.0));
+
+				if choose_mat < 0.8 {
+					// diffuse
+					let albedo = Color::random() * Color::random();
+					sphere_material = Rc::new(Lambertian::new(albedo));
+					world.add(Rc::new(Sphere::new(center, 0.2, sphere_material)));
+				} else if choose_mat < 0.95 {
+					// metal
+					let albedo = Color::random_range(0.5, 1.);
+					let fuzz = random(0., 0.5);
+					sphere_material = Rc::new(Metal::new(albedo, fuzz));
+					world.add(Rc::new(Sphere::new(center, 0.2, sphere_material)));
+				} else {
+					// glass
+					sphere_material = Rc::new(Dielectric::new(1.5));
+					world.add(Rc::new(Sphere::new(center, 0.2, sphere_material)))
+				}
+			}
+		}
+	}
+	let material1 = Rc::new(Dielectric::new(1.5));
+	world.add(Rc::new(Sphere::new(
+		Point3::from(0., 1., 0.),
+		1.,
+		material1,
+	)));
+
+	let material2 = Rc::new(Lambertian::new(Color::from(0.4, 0.2, 0.1)));
+	world.add(Rc::new(Sphere::new(
+		Point3::from(-4., 1., 0.),
+		1.,
+		material2,
+	)));
+
+	let material3 = Rc::new(Metal::new(Color::from(0.7, 0.6, 0.5), 0.0));
+	world.add(Rc::new(Sphere::new(
+		Point3::from(4., 1., 0.),
+		1.,
+		material3,
+	)));
+
+	world
+}
+
 fn ray_color(r: Ray, world: &HittableList, depth: i32) -> Color {
 	let mut rec = HitRecord::default();
 
@@ -120,18 +154,4 @@ fn ray_color(r: Ray, world: &HittableList, depth: i32) -> Color {
 	let t = 0.5 * (unit_direction.y() + 1.0);
 
 	return (1.0 - t) * Color::from(1.0, 1.0, 1.0) + t * Color::from(0.5, 0.7, 1.0);
-}
-
-fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> f64 {
-	let oc = r.origin() - center;
-	let a = r.direction().length_squared();
-	let half_b = Vec3::dot(oc, r.direction());
-	let c = oc.length_squared() - (radius * radius);
-	let discriminant = half_b * half_b - a * c;
-
-	if discriminant < 0.0 {
-		-1.
-	} else {
-		(-half_b - discriminant.sqrt()) / a
-	}
 }
