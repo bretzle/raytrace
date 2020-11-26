c861654c709c198c0f36d66946051009991032d3
bretzle <johnfish218@gmail.com>
2020-11-26 14:47:13 -0600

    Finalized rendering

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ mod vec3;
 const ASPECT_RATIO: f64 = 3.0 / 2.0;
 const IMAGE_WIDTH: i32 = 1200;
 const IMAGE_HEIGHT: i32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as i32;
-const SAMPLES_PER_PIXEL: i32 = 50;
+const SAMPLES_PER_PIXEL: i32 = 500;
 const MAX_DEPTH: i32 = 50;
 
 fn main() {
